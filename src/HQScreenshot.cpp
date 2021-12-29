1dc5ceb1e7a8abb87e10b1bb694cedded89431e4
svenlr <sven.langner@tu-braunschweig.de>
2021-12-29 15:40:05 +0100

    getActualWidth() -> getWidth()

--- src/HQScreenshot.cpp
+++ src/HQScreenshot.cpp
@@ -84,8 +84,8 @@ void HQScreenshot::pre_render_callback() {
                     obj->setRenderQueueGroup(3);
                 }
             });
-            int w = client_camera->getViewport()->getActualWidth() * 4;
-            int h = client_camera->getViewport()->getActualHeight() * 4;
+            int w = client_camera->getViewport()->getWidth() * 4;
+            int h = client_camera->getViewport()->getHeight() * 4;
             rtt_img.initialize("hq_screenshot", client_camera, w, h);
         }
     } else { // scene_manager == nullptr
